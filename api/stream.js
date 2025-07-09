b36b5f9eef7db56a54b6a30a90aa1404ba6dfc07
kyzotom <Kyzo@post.sk>
2025-07-09 20:56:11 +0200

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,10 +1,8 @@
-module.exports = (req, res) => {
-    res.json({
-        streams: [
-            {
-                title: "Test Stream (DEMO)",
-                url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-            }
-        ]
-    });
-};
+export default function handler(req, res) {
+  res.status(200).json({
+    streams: [{
+      title: "Test Stream (DEMO)",
+      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    }]
+  });
+}
