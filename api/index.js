c815527549fbeaab45f2d2567ec516b3bd1d4361
kyzotom <Kyzo@post.sk>
2025-07-09 18:40:30 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,37 @@
-module.exports = (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ ok: true, msg: "Stremio Addon demo funguje!" }));
+const { addonBuilder } = require("stremio-addon-sdk");
+
+const manifest = {
+  id: "org.demo.addon",
+  version: "1.0.0",
+  name: "Demo Addon",
+  description: "Demo pre Stremio Vercel nasadenie.",
+  catalogs: [
+    {
+      type: "movie",
+      id: "demo-catalog",
+      name: "Demo Catalog",
+      extra: [{ name: "search" }]
+    }
+  ],
+  resources: ["catalog", "stream"],
+  types: ["movie"],
+  idPrefixes: ["tt"]
 };
+
+const builder = new addonBuilder(manifest);
+
+builder.defineCatalogHandler(() => Promise.resolve({ metas: [] }));
+
+builder.defineStreamHandler(() =>
+  Promise.resolve({
+    streams: [
+      {
+        title: "Demo Test Stream",
+        url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+      }
+    ]
+  })
+);
+
+// TOTO je dôležité pre Vercel!
+module.exports = (req, res) => builder.getInterface()(req, res);
